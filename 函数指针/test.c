d15dc0ed2882e449db718c060f0d5c59d2341920
farewell1234567 <91715607+farewell1234567@users.noreply.github.com>
2022-02-23 23:46:17 +0800

    函数指针

--- 函数指针/test.c
+++ 函数指针/test.c
@@ -1,2 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
 
+//函数指针：存放函数地址的指针
+
+int Add(int a, int b)
+{
+	return a + b;
+}
+
+int main()
+{
+	int (*pf1)(int ,int) = &Add;
+	int (*pf2)(int, int) = Add;
+	//两种表达方式都可以，Add等价于&Add
+
+	//函数指针的调用
+	int i = pf1(3, 5);
+	int j = (*pf1)(3, 5);
+	//这两种表达方式都可以，因为pf1和Add都表示函数的地址，解引用符号没有意义
+	printf("%d %d\n", i, j);
+	return 0;
+}
